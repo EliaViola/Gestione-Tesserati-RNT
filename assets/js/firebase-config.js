e9560286047a987cb18876d7e9801e2e4ad62b0a
EliaViola <64260895+EliaViola@users.noreply.github.com>
2025-06-03 13:25:02 +0200

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,17 @@
+// assets/js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBVcNJhXiytEKBtC09T3kbykVzAY0AHZmM",
+  authDomain: "rari-nantes-tesserati.firebaseapp.com",
+  projectId: "rari-nantes-tesserati",
+  storageBucket: "rari-nantes-tesserati.appspot.com",
+  messagingSenderId: "435337228811",
+  appId: "1:435337228811:web:a5f1fd0fb9e17bfbc00d0e",
+  measurementId: "G-558LQEWD8Z"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Export per utilizzo in altri file se necessario
+export { firebaseApp, db };
