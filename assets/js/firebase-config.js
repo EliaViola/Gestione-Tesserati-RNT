8f31dad92411f78ae24df2c9089f972b5f5a0329
EliaViola <64260895+EliaViola@users.noreply.github.com>
2025-06-05 13:36:11 +0200

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -7,7 +7,6 @@ const firebaseConfig = {
   storageBucket: "rari-nantes-tesserati.appspot.com",
   messagingSenderId: "435337228811",
   appId: "1:435337228811:web:a5f1fd0fb9e17bfbc00d0e",
-  measurementId: "G-558LQEWD8Z"
 };
 
 // Inizializza Firebase solo se non già inizializzato
