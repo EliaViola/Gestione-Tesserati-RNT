04db0b3bab32d7d1d73169e48e65008bd4668998
EliaViola <64260895+EliaViola@users.noreply.github.com>
2025-06-09 13:41:43 +0200

    Update gestione-corsi.js

--- assets/js/gestione-corsi.js
+++ assets/js/gestione-corsi.js
@@ -1,13 +1,3 @@
-// Configurazione Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBVcNJhXiytEKBtC09T3kbykVzAY0AHZmM",
-  authDomain: "rari-nantes-tesserati.firebaseapp.com",
-  projectId: "rari-nantes-tesserati",
-  storageBucket: "rari-nantes-tesserati.appspot.com",
-  messagingSenderId: "435337228811",
-  appId: "1:435337228811:web:a5f1fd0fb9e17bfbc00d0e"
-};
-
 // Inizializza Firebase solo se non è già stata inizializzata
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
